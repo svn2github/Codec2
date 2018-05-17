a10c474635a49bdba3b3e36529064bac39612ba3
hobbes1069 <hobbes1069@01035d8c-6547-0410-b346-abe4f91aad63>
2018-05-17 14:08:07 +0000

    Branching and tagging of FreeDV 1.3 and codec2 0.8.
    git-svn-id: https://svn.code.sf.net/p/freetel/code/codec2@3612 01035d8c-6547-0410-b346-abe4f91aad63

---
+++ tags/1.3/README.txt
@@ -0,0 +1,204 @@
+==================================
+ FreeDV GUI README.txt
+==================================
+
+This document describes how to build the FreeDV GUI program for
+various operating systems.  See also:
+
+  http://freedv.org - introduction, documentation, downloads
+  RELEASE_NOTES.txt - changes made to each version
+  USER_MANUAL.txt   - FreeDV GUI Manual
+
+==================================
+ Building and installing on Linux
+==================================
+
+First the basics:
+
+  $ sudo apt-get install build-essential cmake subversion
+
+To install the required development libraries instead of building them
+statically:
+
+Debian/Ubuntu:
+
+  $ sudo apt-get install libwxgtk3.0-dev portaudio19-dev \
+    libhamlib-dev libsamplerate-dev libasound2-dev libao-dev libgsm1-dev \
+    libsndfile-dev
+
+Fedora:
+
+  $ sudo dnf install wxGTK3-devel portaudio-devel libsamplerate-devel \ 
+    libsndfile-devel speexdsp-devel hamlib-devel alsa-lib-devel libao-devel \
+    gsm-devel
+
+
+RHEL/CentOS and derivitves (requires Fedora EPEL repository)
+
+  $ sudo yum install wxGTK3-devel portaudio-devel libsamplerate-devel \ 
+    libsndfile-devel speexdsp-devel hamlib-devel alsa-lib-devel libao-devel \
+    gsm-devel
+
+
+Quickstart 1
+------------
+
+Using a modern Linux, and the development library packages installed above:
+
+  $ cd /path/to/freedv
+  $ mkdir build_linux
+  $ cd build_linux
+  $ cmake ../ 
+  $ make
+  $ ./src/freedv
+
+Quickstart 2
+-------------
+
+Using a local build of codec2-dev that you may be developing:
+
+  $ cd /path/to/freedv
+  $ mkdir build_linux
+  $ cd build_linux
+  $ cmake -DCMAKE_BUILD_TYPE=Debug -DCODEC2_BUILD_DIR=/path/to/codec2-dev/build_linux  ..
+  $ make
+  $ ./src/freedv
+
+Quickstart 3
+------------
+
+Builds static versions of wxWidgets, portaudio, codec2-dev, which are commonly
+missing on older Linux systems.
+
+1/ Assumes static build of wxWidgets and the freedv-dev source is checked out into ~/freedv-dev:
+
+  $ cd ~/freedv-dev
+  $ mkdir build_linux
+  $ cd build_linux
+  $ cmake -DBOOTSTRAP_WXWIDGETS=TRUE ../
+  $ make
+
+2/ Then you can configure FreeDV using your local codec-dev, something like:
+
+  $ cmake -DCMAKE_BUILD_TYPE=Debug -DBOOTSTRAP_WXWIDGETS=TRUE -DCODEC2_BUILD_DIR=/path/to/codec2-dev/build_linux -DUSE_STATIC_PORTAUDIO=TRUE ../
+
+3/ OR checkout and build a copy of codec2-dev:
+
+  $ cmake -DBOOTSTRAP_WXWIDGETS=TRUE -DUSE_STATIC_CODEC2=TRUE -DUSE_STATIC_PORTAUDIO=TRUE  ../
+  
+4/ Build and run FreeDV:
+
+   $ make
+   $ ./src/freedv
+
+=======================================================
+ Building for Windows on Fedora 28 (Cross compiling)
+=======================================================
+
+Install basic MinGW development packages:
+  $ sudo dnf install mingw{32,64}-filesystem mingw{32,64}-binutils \
+    mingw{32,64}-gcc mingw{32/64}-crt mingw{32,64}-headers
+
+Install dependencies of FreeDV/Codec2:
+  $ sudo dnf install mingw{32,64}-speex
+
+Enable Freetel specific packages not currently in Fedora proper:
+  $ sudo dnf copr enable hobbes1069/mingw
+  $ sudo dnf install mingw{32,64}-wxWidgets3 mingw{32,64}-hamlib \
+    mingw{32,64}-portaudio mingw{32,64}-libsndfile
+
+Building FreeDV:
+  $ cd ~/freedv-dev
+  $ mkdir build_windows && cd build_windows
+  $ mingw64-cmake
+  $ make
+  $ make package
+  
+WANTED: Instructions for cross compiling on Ubuntu 17 or 18
+
+====================================
+ Building and installing on Windows
+====================================
+
+The windows build is similar to linux and follows the same basic workflow,
+however, while codec2 and FreeDV (freedv) build well on windows, some of the
+dependencies do not. For that reson current windows releases are cross-compiled
+from linux.
+
+Only MinGW is supported. While it is likely possible to perform a pure MinGW
+build, installing MSYS2 will make your life easier.
+
+CMake may not automatically detect that you're in the MSYS environment. If this
+occurs you need to pass cmake the proper generator:
+
+cmake -G"MSYS Makefiles" [other options] </path/to/source>
+
+===============================
+ Bootstrapping wxWidgets build
+===============================
+
+If wxWidgets (>= 3.0) is not available then one option is to have CMake boot-
+strap the build for FreeDV.
+
+This is required because the tool wx-config is used to get the correct compiler
+and linker flags of the wxWidgets components needed by FreeDV. Since this is
+normally done at configure time, not during "make", it is not possible for CMake
+or have this information prior to building wxWidgets.
+
+In order to work around this issue you can "bootstrap" the wxWidgets build using
+the CMake option, "BOOTSTRAP_WXWIDGETS". wxWidgets will be built using static
+libraries.
+
+NOTE: This forces "USE_STATIC_WXWIDGETS" to be true internally regarless of the
+value set manually.
+
+(from any directory, but empty directory outside of the source is prefered.)
+$ cmake -DBOOTSTRAP_WXWIDGETS=TRUE /path/to/freedv
+$ make
+(wxWidgets is downloaded and built)
+$ cmake .
+(wxWidgets build should be detected)
+$ make
+(if all goes well, as root)
+$ make install
+
+====================================
+ Building and installing on OSX
+====================================
+
+Pls see README.osx
+
+====================================
+ Building and installing on FreeBSD
+====================================
+
+As per "Quickstart 2" above but change build_linux to build_freebsd
+
+=======
+Editing
+=======
+
+Please make sure your text editor does not insert tabs, and
+used indents of 4 spaces.  The following .emacs code was used to
+configure emacs:
+
+(setq-default indent-tabs-mode nil)
+
+(add-hook 'c-mode-common-hook
+          (function (lambda ()
+                      (setq c-basic-offset 4)
+                      )))
+
+===============
+    TODO
+===============
+
+This software needs some maintenance and refactoring.  Patches
+welcome!
+
+[ ] Remove unnecessary PortAudio Wrapper layer
+[ ] break fdmdv2_main.cpp into smaller files
+[ ] rename src files from fdmdv2_ -> freedv_
+[ ] profile code and explore optimisations
+
+
---
+++ tags/1.3/src/serialport.cpp
@@ -0,0 +1,340 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include "serialport.h"
+
+#ifdef _WIN32
+#include <strsafe.h>
+#endif
+
+Serialport::Serialport() {
+    com_handle = COM_HANDLE_INVALID;
+}
+
+Serialport::~Serialport() {
+    if (isopen()) {
+        closeport();
+    }
+}
+
+// returns true if comm port opened OK, false if there was a problem
+
+bool Serialport::openport(const char name[], bool useRTS, bool RTSPos, bool useDTR, bool DTRPos)
+{
+    fprintf(stderr, "starting openport(), name: %s strlen(name): %d useRTS: %d RTSPos: %d useDTR: %d DTRPos: %d\n",
+            name, (int)strlen(name), useRTS, RTSPos, useDTR, DTRPos);
+
+    if (com_handle != COM_HANDLE_INVALID) {
+        closeport();
+        fprintf(stderr, "comm_handle invalid, closing\n");
+    }
+
+    m_useRTS = useRTS;
+    m_RTSPos = RTSPos;
+    m_useDTR = useDTR;
+    m_DTRPos = DTRPos;
+
+    
+#ifdef _WIN32
+    {
+        COMMTIMEOUTS timeouts;
+        DCB	dcb;
+        TCHAR  lpszFunction[100];
+	
+        // As per:
+        //   [1] https://support.microsoft.com/en-us/help/115831/howto-specify-serial-ports-larger-than-com9
+        //   [2] Hamlib lib/termios.c, win32_serial_open()
+
+	/*  
+            To test change of COM port for USB serial device on Windows
+
+            1/ Run->devmgmnt.msc
+            2/ Change COM port Ports (COM & LPT) -> Serial Device -> Properties Tab -> Advanced
+            3/ Unplug USB serial devce and plug in again.  This is really important.  FreeDV won't recognise
+               new COM port number until this is done.
+            4/ Test PTT on FreeDV Tools->PTT
+	*/
+
+        TCHAR  nameWithStrangePrefix[100];
+        StringCchPrintf(nameWithStrangePrefix, 100, "\\\\.\\%s", name);
+        fputs("nameWithStrangePrefix: ", stderr);
+	fputs(nameWithStrangePrefix, stderr);
+	fprintf(stderr,"\n");
+
+#ifdef NOT_USED
+
+        COMMCONFIG CC;
+        DWORD CCsize=sizeof(CC);
+	memset(&CC, 0, CCsize);
+	CC.dwSize = CCsize;
+
+	/* Commented out by David May 13 2018, as it was failing after
+           "name" changed to "nameWithStrangePrefix" that is reqd for
+           support of COM ports above 9.  I am not sure if this is
+           needed as I can't see anything similar in Hamlib */
+	
+        if(GetDefaultCommConfigA(nameWithStrangePrefix, &CC, &CCsize)) {
+	    fprintf(stderr, "GetDefaultCommConfigA OK\n");         
+            CC.dcb.fOutxCtsFlow		= FALSE;
+            CC.dcb.fOutxDsrFlow		= FALSE;
+            CC.dcb.fDtrControl		= DTR_CONTROL_DISABLE;
+            CC.dcb.fDsrSensitivity	= FALSE;
+            CC.dcb.fRtsControl		= RTS_CONTROL_DISABLE;
+            if (!SetDefaultCommConfigA(nameWithStrangePrefix, &CC, CCsize)) {
+		StringCchPrintf(lpszFunction, 100, "%s", "SetDefaultCommConfigA");
+                goto error;
+            }
+            fprintf(stderr, "SetDefaultCommConfigA OK\n");
+        } else {
+	     StringCchPrintf(lpszFunction, 100, "%s", "GetDefaultCommConfigA");
+             goto error;
+        }
+#endif
+	
+        if((com_handle=CreateFileA(nameWithStrangePrefix
+                                   ,GENERIC_READ | GENERIC_WRITE/* Access */
+                                   ,0				/* Share mode */
+                                   ,NULL		 	/* Security attributes */
+                                   ,OPEN_EXISTING		/* Create access */
+                                   ,0                           /* File attributes */
+                                   ,NULL		        /* Template */
+                                   ))==INVALID_HANDLE_VALUE) {
+	    StringCchPrintf(lpszFunction, 100, "%s", "CreateFileA");
+	    goto error;
+	}
+        fprintf(stderr, "CreateFileA OK\n");
+	
+        if(GetCommTimeouts(com_handle, &timeouts)) {
+ 	    fprintf(stderr, "GetCommTimeouts OK\n");
+            timeouts.ReadIntervalTimeout=MAXDWORD;
+            timeouts.ReadTotalTimeoutMultiplier=0;
+            timeouts.ReadTotalTimeoutConstant=0;		// No-wait read timeout
+            timeouts.WriteTotalTimeoutMultiplier=0;
+            timeouts.WriteTotalTimeoutConstant=5000;	// 5 seconds
+	    if (!SetCommTimeouts(com_handle,&timeouts)) {
+	      StringCchPrintf(lpszFunction, 100, "%s", "SetCommTimeouts");
+              goto error;	      
+	    }
+	    fprintf(stderr, "SetCommTimeouts OK\n");
+        } else {
+	    StringCchPrintf(lpszFunction, 100, "%s", "GetCommTimeouts");
+            goto error;
+        }
+
+        /* Force N-8-1 mode: */
+        if(GetCommState(com_handle, &dcb)==TRUE) {
+	    fprintf(stderr, "GetCommState OK\n");
+	    
+            dcb.ByteSize		= 8;
+            dcb.Parity			= NOPARITY;
+            dcb.StopBits		= ONESTOPBIT;
+            dcb.DCBlength		= sizeof(DCB);
+            dcb.fBinary			= TRUE;
+            dcb.fOutxCtsFlow	= FALSE;
+            dcb.fOutxDsrFlow	= FALSE;
+            dcb.fDtrControl		= DTR_CONTROL_DISABLE;
+            dcb.fDsrSensitivity	= FALSE;
+            dcb.fTXContinueOnXoff= TRUE;
+            dcb.fOutX			= FALSE;
+            dcb.fInX			= FALSE;
+            dcb.fRtsControl		= RTS_CONTROL_DISABLE;
+            dcb.fAbortOnError	= FALSE;
+            if (!SetCommState(com_handle, &dcb)) {
+  	        StringCchPrintf(lpszFunction, 100, "%s", "SetCommState");
+                goto error;           
+            }
+	    fprintf(stderr, "SetCommState OK\n");
+        } else {
+  	    StringCchPrintf(lpszFunction, 100, "%s", "GetCommState");
+            goto error;           
+        }
+	
+	return true;
+	
+    error:
+	fprintf(stderr, "%s failed\n", lpszFunction);
+	
+        // Retrieve the system error message for the last-error code
+
+        LPVOID lpMsgBuf;
+        LPVOID lpDisplayBuf;
+        DWORD dw = GetLastError(); 
+
+        FormatMessage(
+                      FORMAT_MESSAGE_ALLOCATE_BUFFER | 
+                      FORMAT_MESSAGE_FROM_SYSTEM |
+                      FORMAT_MESSAGE_IGNORE_INSERTS,
+                      NULL,
+                      dw,
+                      MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+                      (LPTSTR) &lpMsgBuf,
+                      0, NULL );
+
+        // Display the error message
+
+        lpDisplayBuf = (LPVOID)LocalAlloc(LMEM_ZEROINIT, 
+                                          (lstrlen((LPCTSTR)lpMsgBuf) + lstrlen((LPCTSTR)lpszFunction) + 40) * sizeof(TCHAR)); 
+        StringCchPrintf((LPTSTR)lpDisplayBuf, 
+                        LocalSize(lpDisplayBuf) / sizeof(TCHAR),
+                        TEXT("%s failed with error %d: %s"), 
+                        lpszFunction, dw, lpMsgBuf); 
+        MessageBox(NULL, (LPCTSTR)lpDisplayBuf, TEXT("Error"), MB_OK); 
+
+        LocalFree(lpMsgBuf);
+        LocalFree(lpDisplayBuf);
+
+	return false;
+    }
+#else
+	{
+		struct termios t;
+
+		if((com_handle=open(name, O_NONBLOCK|O_RDWR))== COM_HANDLE_INVALID)
+			return false;
+
+		if(tcgetattr(com_handle, &t)==-1) {
+			close(com_handle);
+			com_handle = COM_HANDLE_INVALID;
+			return false;
+		}
+
+		t.c_iflag = (
+					  IGNBRK   /* ignore BREAK condition */
+					| IGNPAR   /* ignore (discard) parity errors */
+					);
+		t.c_oflag = 0;	/* No output processing */
+		t.c_cflag = (
+					  CS8         /* 8 bits */
+					| CREAD       /* enable receiver */
+
+		/*
+		Fun snippet from the FreeBSD manpage:
+
+			 If CREAD is set, the receiver is enabled.  Otherwise, no character is
+			 received.  Not all hardware supports this bit.  In fact, this flag is
+			 pretty silly and if it were not part of the termios specification it
+			 would be omitted.
+		*/
+					| CLOCAL      /* ignore modem status lines */
+					);
+
+		t.c_lflag = 0;	/* No local modes */
+		if(tcsetattr(com_handle, TCSANOW, &t)==-1) {
+			close(com_handle);
+			com_handle = COM_HANDLE_INVALID;
+			return false;
+		}
+		
+	}
+#endif
+	return true;
+}
+
+
+// fixme: this takes about one second to close under Linux
+
+void Serialport::closeport()
+{
+#ifdef _WIN32
+	CloseHandle(com_handle);
+#else
+	close(com_handle);
+#endif
+        com_handle = COM_HANDLE_INVALID;
+}
+
+//----------------------------------------------------------------
+// (raise|lower)(RTS|DTR)()
+//
+// Raises/lowers the specified signal
+//----------------------------------------------------------------
+
+void Serialport::raiseDTR(void)
+{
+	if(com_handle == COM_HANDLE_INVALID)
+		return;
+#ifdef _WIN32
+	EscapeCommFunction(com_handle, SETDTR);
+#else
+	{	// For C89 happiness
+		int flags = TIOCM_DTR;
+		ioctl(com_handle, TIOCMBIS, &flags);
+	}
+#endif
+}
+
+void Serialport::raiseRTS(void)
+{
+	if(com_handle == COM_HANDLE_INVALID)
+		return;
+#ifdef _WIN32
+	EscapeCommFunction(com_handle, SETRTS);
+#else
+	{	// For C89 happiness
+		int flags = TIOCM_RTS;
+		ioctl(com_handle, TIOCMBIS, &flags);
+	}
+#endif
+}
+
+void Serialport::lowerDTR(void)
+{
+	if(com_handle == COM_HANDLE_INVALID)
+		return;
+#ifdef _WIN32
+	EscapeCommFunction(com_handle, CLRDTR);
+#else
+	{	// For C89 happiness
+		int flags = TIOCM_DTR;
+		ioctl(com_handle, TIOCMBIC, &flags);
+	}
+#endif
+}
+
+void Serialport::lowerRTS(void)
+{
+	if(com_handle == COM_HANDLE_INVALID)
+		return;
+#ifdef _WIN32
+	EscapeCommFunction(com_handle, CLRRTS);
+#else
+	{	// For C89 happiness
+		int flags = TIOCM_RTS;
+		ioctl(com_handle, TIOCMBIC, &flags);
+	}
+#endif
+}
+
+void Serialport::ptt(bool tx) {
+
+   /*  Truth table:
+
+          g_tx   RTSPos   RTS
+          -------------------
+          0      1        0
+          1      1        1
+          0      0        1
+          1      0        0
+
+          exclusive NOR
+    */
+
+    if (com_handle != COM_HANDLE_INVALID) {
+        if (m_useRTS) {
+            //fprintf(stderr, "g_tx: %d m_boolRTSPos: %d serialLine: %d\n", g_tx, wxGetApp().m_boolRTSPos, g_tx == wxGetApp().m_boolRTSPos);
+            if (tx == m_RTSPos)
+                raiseRTS();
+            else
+                lowerRTS();
+        }
+        if (m_useDTR) {
+            //fprintf(stderr, "g_tx: %d m_boolDTRPos: %d serialLine: %d\n", g_tx, wxGetApp().m_boolDTRPos, g_tx == wxGetApp().m_boolDTRPos);
+            if (tx == m_DTRPos)
+                raiseDTR();
+            else
+                lowerDTR();
+        }
+ 
+    }
+}
